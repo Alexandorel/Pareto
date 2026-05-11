c21f707412658509637ce4d55195b14aeca88926
Alex <vintalexandru03@gmail.com>
2026-05-11 13:52:48 +0300

    fix text color on darkmode

--- obj/Debug/net10.0/Pareto.AssemblyInfo.cs
+++ obj/Debug/net10.0/Pareto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pareto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b66d0d12d4a8423568123913becfcbced2feef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2eefe43084cc4afc7f2c372055d1520f0d422e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pareto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pareto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
